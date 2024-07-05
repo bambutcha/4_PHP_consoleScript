bad4a8c636f6d5866e7dcdafe9c78c78f9ab9e63
bambutcha <stringdeity@gmail.com>
2024-07-05 17:12:07 +0300

    Create autoloader.php Edit all files Move classes to App folder
    [#master]

--- autoloader.php
+++ autoloader.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< HEAD
 
 function requireDir(string $dirName) {
     $dirFiles = scandir($dirName);
@@ -16,3 +17,16 @@ function requireDir(string $dirName) {
 }
 
 requireDir(__DIR__ . DIRECTORY_SEPARATOR . 'App');
+=======
+requireDir('App');
+
+function requireDir(string $dirName) {
+    $dirFiles = scandir($dirName);
+    array_shift($dirFiles);
+    array_shift($dirFiles);
+
+    foreach ($dirFiles as $file) {
+        require __DIR__ . DIRECTORY_SEPARATOR .implode(DIRECTORY_SEPARATOR, [$dirName, $file]);
+    }
+}
+>>>>>>> 8ceff2e (Create autoloader.php)
