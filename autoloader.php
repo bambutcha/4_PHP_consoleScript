af2a930e021638330e39168749adde5d60edf816
bambutcha <stringdeity@gmail.com>
2024-07-10 17:31:25 +0300

    Edit autoloader.php index.php Move Counter.php to Handler folder
    [#master]

--- autoloader.php
+++ autoloader.php
@@ -1,12 +1,39 @@
 <?php
-requireDir('App');
 
-function requireDir(string $dirName) {
+/**function requireDir($dirName, $className)
+{
     $dirFiles = scandir($dirName);
-    array_shift($dirFiles);
-    array_shift($dirFiles);
 
     foreach ($dirFiles as $file) {
-        require __DIR__ . DIRECTORY_SEPARATOR .implode(DIRECTORY_SEPARATOR, [$dirName, $file]);
+        if ($file !== '.' && $file !== '..') {
+            $filePath = $dirName . DIRECTORY_SEPARATOR . $file;
+            if (is_dir($filePath)) {
+                requireDir($filePath, $className);
+            } elseif (is_file($filePath) && basename($filePath, '.php') === $className) {
+                require_once $filePath;
+            }
+        }
     }
 }
+ **/
+
+function requireDir($dirName)
+{
+    $dirFiles = scandir($dirName);
+
+    foreach ($dirFiles as $file) {
+        if ($file !== '.' && $file !== '..') {
+            $filePath = $dirName . DIRECTORY_SEPARATOR . $file;
+            if (is_dir($filePath)) {
+                requireDir($filePath);
+            } elseif (is_file($filePath)) {
+                require_once $filePath;
+            }
+        }
+    }
+}
+
+// Вызываем функцию для загрузки всех файлов в директории 'App'
+requireDir(__DIR__ . DIRECTORY_SEPARATOR . 'App');
+
+
