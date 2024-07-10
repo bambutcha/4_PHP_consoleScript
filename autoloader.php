82aa9337a836b6eac41e8c7e5ff294880150b21d
bambutcha <stringdeity@gmail.com>
2024-07-10 19:45:00 +0300

    Revert "Add Handler folder in App folder"
    This reverts commit acfa41dbb5913de2c4133d3e76aeb30972d8d911.

--- autoloader.php
+++ autoloader.php
@@ -1,18 +1,12 @@
 <?php
+requireDir('App');
 
 function requireDir(string $dirName) {
     $dirFiles = scandir($dirName);
+    array_shift($dirFiles);
+    array_shift($dirFiles);
 
     foreach ($dirFiles as $file) {
-        if ($file !== '.' && $file !== '..') {
-            $filePath = $dirName . DIRECTORY_SEPARATOR . $file;
-            if (is_dir($filePath)) {
-                requireDir($filePath);
-            } elseif (is_file($filePath)) {
-                require_once $filePath;
-            }
-        }
+        require __DIR__ . DIRECTORY_SEPARATOR .implode(DIRECTORY_SEPARATOR, [$dirName, $file]);
     }
 }
-
-requireDir(__DIR__ . DIRECTORY_SEPARATOR . 'App');
